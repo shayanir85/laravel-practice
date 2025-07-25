e29ee3cadb39ef92e59469b78cd55c08c66cf6b2
shayanir85 <shayaniranpor8@gmail.com>
2025-07-25 19:22:31 +0330

    added some tables to database and controllers

---
+++ database/migrations/2025_07_25_135812_create_books_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('writer');
+            $table->date('year');
+            $table->timestamps();
+
+            // Add this AFTER creating all other columns
+            $table->foreignId('genre_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
